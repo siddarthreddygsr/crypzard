28974248daada7533c03d34dfa5980f372ac8ace
siddarthreddygsr <siddarthreddygsr@gmail.com>
2023-10-02 22:28:51 +0530

    added firewall to clientUI

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ![UI Image](images/UI.png)
+![UI Image](images/UIImage.png)
+![UI Image](images/updates.png)
 ![Firewall UI](images/firewallUI.png)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os,subprocess
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets
 from qt_material import apply_stylesheet
 from qtwidgets import Toggle
 
@@ -31,9 +32,45 @@ class NetworkPage(QWidget):
         ufw_section_layout.addStretch(1)  # Pushes the toggle button to the right
         network_layout.addLayout(ufw_section_layout)
 
-        # Add other content to the NetworkPage
-        for i in range(2):  # Add some content to demonstrate scrolling
-            network_layout.addWidget(QLabel(f"Network Page Content {i}"))
+        self.status_label = QLabel("Firewall Status: ", self)
+        self.status_label.setGeometry(20, 20, 200, 30)
+
+        # Create buttons for various actions
+        enable_button = QPushButton("Enable Firewall", self)
+        enable_button.setGeometry(20, 60, 150, 30)
+        enable_button.clicked.connect(self.enable_firewall)
+
+        disable_button = QPushButton("Disable Firewall", self)
+        disable_button.setGeometry(200, 60, 150, 30)
+        disable_button.clicked.connect(self.disable_firewall)
+
+        self.reset_button = QPushButton("Reset Firewall", self)
+        self.reset_button.setGeometry(380, 60, 150, 30)
+        self.reset_button.clicked.connect(self.reset_firewall)
+
+        self.list_rules_button = QPushButton("List Rules", self)
+        self.list_rules_button.setGeometry(20, 100, 150, 30)
+        self.list_rules_button.clicked.connect(self.list_rules)
+
+        self.add_rule_button = QPushButton("Add Rule", self)
+        self.add_rule_button.setGeometry(200, 100, 150, 30)
+        self.add_rule_button.clicked.connect(self.add_rule)
+
+        self.delete_rule_button = QPushButton("Delete Rule", self)
+        self.delete_rule_button.setGeometry(380, 100, 150, 30)
+        self.delete_rule_button.clicked.connect(self.delete_rule)
+
+        # Create a QTextEdit widget to display rule listing and adding
+        self.rules_text_edit = QTextEdit(self)
+        self.rules_text_edit.setGeometry(20, 150, 550, 200)
+
+        # network_layout.addWidget(self.status_label)
+        # network_layout.addWidget(enable_button)
+        # network_layout.addWidget(disable_button)
+        network_layout.addWidget(self.list_rules_button)
+        network_layout.addWidget(self.add_rule_button)
+        network_layout.addWidget(self.delete_rule_button)
+        network_layout.addWidget(self.rules_text_edit)
 
         content_widget = QWidget()  # Create a widget to hold the content
         content_widget.setLayout(network_layout)
@@ -68,6 +105,69 @@ class NetworkPage(QWidget):
                 subprocess.run(["sudo", "ufw", "disable"])
         except subprocess.CalledProcessError:
             pass  # Handle errors as needed
+    
+    def reset_firewall(self):
+        subprocess.run(["sudo","ufw","--force","reset"])
+
+        self.update_status_label()
+        
+
+    def enable_firewall(self):
+        # Run the 'ufw enable' command as a subprocess with sudo
+        subprocess.run(["sudo", "ufw", "enable"])
+
+        # Update the status label to display the new firewall status
+        self.update_status_label()
+
+    def disable_firewall(self):
+        # Run the 'ufw disable' command as a subprocess with sudo
+        subprocess.run(["sudo", "ufw", "disable"])
+
+        # Update the status label to display the new firewall status
+        self.update_status_label()
+
+    def list_rules(self):
+        # Run the 'ufw status' command to list firewall rules
+        rules_output = subprocess.getoutput("sudo ufw status")
+        self.rules_text_edit.setPlainText(rules_output)
+
+    def add_rule(self):
+        # Open a dialog to input a new rule
+        new_rule, ok = QtWidgets.QInputDialog.getText(
+            self, "Add Rule", "Enter a new firewall rule (e.g., allow 80/tcp):"
+        )
+        new_rule = new_rule.split()
+        precommand = ['sudo','ufw']
+        precommand.extend(new_rule)
+        print(precommand)
+        if ok:
+            # Run the 'ufw allow' or 'ufw deny' command with the new rule
+            subprocess.run(precommand)
+
+            # Update the displayed rules
+            self.list_rules()
+
+    def delete_rule(self):
+        # Open a dialog to input the rule to be deleted
+        rule_to_delete, ok = QtWidgets.QInputDialog.getText(
+            self, "Delete Rule", "Enter the firewall rule to delete:"
+        )
+        rule_to_delete = rule_to_delete.split()
+        precommand = ['sudo','ufw']
+        precommand.extend(rule_to_delete)
+        if ok:
+            # Run the 'ufw delete' command to delete the specified rule
+            subprocess.run(rule_to_delete)
+
+            # Update the displayed rules
+            self.list_rules()
+
+    def update_status_label(self):
+        # Retrieve the current firewall status using 'ufw status' command
+        status = subprocess.getoutput("sudo ufw status | head -1")
+        status = status.split()[-1]
+        # Update the status label text to reflect the current status
+        self.status_label.setText(f"Firewall Status: {status}")
 
 
 
